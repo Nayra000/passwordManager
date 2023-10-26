e9504077afc436f60786e9542456ae88031c1f4d
mhmadalaa <mhmaaddalaa@gmail.com>
2023-10-26 23:26:37 +0200

    feat: add basic database schemas

---
+++ models/userModel.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const { default: isEmail } = require('validator/lib/isEmail');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User must have a name'],
+    minlength: [5, 'User name must be at least 5 characters'],
+    maxlength: [15, 'User name must be at most 15 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: isEmail,
+      message: 'Enter a valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'User must have a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    minlength: 8,
+    validate: {
+      // this validator function only works with ""SAVE""
+      // so we need to use save in case of update or similars
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: 'Please, check if password is the same.',
+    },
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined; // to be deleted after check if it's equal
+  this.passwordChangedAt = Date.now();
+
+  next();
+});
+
+// helper functions for authentication
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    return this.passwordChangedAt.getTime() / 1000 > JWTTimestamp;
+  }
+
+  return false;
+};
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model('User', userSchema);
