e82cc6eab5ef06b485f673a92a76c3cac26ab465
lilhind <130204568+lilhind@users.noreply.github.com>
2023-10-28 12:39:00 +0000

    changed to api directory

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const viewRouter = require('./routers/viewsRouter');
+const viewRouter = require('../routers/viewsRouter');
 
 const app = express();
 
